7714fac59134b1c9c887ec599ddb99e01095be5f
bhoomikahemanth <48724256+bhoomikahemanth@users.noreply.github.com>
2020-04-28 11:15:37 +0100

    Commit Static Polymorphism Code

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticPolyTest
+{
+    class Program
+    {
+        void print (int i)
+        {
+            Console.WriteLine("Printing int: "+i);
+        }
+
+        void print(double f)
+        {
+            Console.WriteLine("Printing int: {0}", f);
+        }
+
+        void print(string s)
+        {
+            Console.WriteLine("Printing int: {0}", s);
+        }
+
+        static void Main(string[] args)
+        {
+
+            Program p = new Program();
+
+            p.print(5);
+
+            p.print(67.89);
+
+            p.print("Hey!....");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
