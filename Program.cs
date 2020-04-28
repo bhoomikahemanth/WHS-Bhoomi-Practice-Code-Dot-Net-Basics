ba5364bfed2a0353b978cd85ea51df1fda2d27c3
bhoomikahemanth <48724256+bhoomikahemanth@users.noreply.github.com>
2020-04-28 11:13:39 +0100

    Commit Files Code

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // File Class => System.IO
+            // File Methods
+
+            // store in a variable & pass it to the File Method
+            string writeTextString = "Hello, World!";
+
+            // path & string
+            File.WriteAllText("filename.txt", writeTextString);
+
+            string readTextstring = File.ReadAllText("filename.txt");
+            Console.WriteLine(readTextstring);
+
+            Console.ReadKey();
+
+        }
+    }
+}
