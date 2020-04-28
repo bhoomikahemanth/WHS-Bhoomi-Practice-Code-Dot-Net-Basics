8d1b3509a0ea3eab42a2f91312a13273eb2dfa3d
bhoomikahemanth <48724256+bhoomikahemanth@users.noreply.github.com>
2020-04-28 11:05:50 +0100

    Commit Function Overloading

---
+++ MathOperation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNCSDNFuncOverLoad
+{
+    class MathOperation
+    {
+
+        public int Add(int a, int b)
+        {
+            Console.WriteLine("Add Method with 2 int parameters.");
+            return a + b;
+                        
+        }
+
+        public int Add(int a, int b, int c)
+        {
+            Console.WriteLine("Add Method with 3 int parameters.");
+            return a + b + c;
+        }
+
+        public double Add(double a, double b, double c, double d)
+        {
+            Console.WriteLine("Add Method with 4 double parameters.");
+            return a + b + c + d;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BNCSDNFuncOverLoad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MathOperation mo = new MathOperation();
+
+            int res1 = mo.Add(1, 2);
+            Console.WriteLine(res1);
+
+            int res2 = mo.Add(1, 2, 3);
+            Console.WriteLine(res2);
+
+            double res3 = mo.Add(1.2, 2.3, 3.4, 4.5);
+            Console.WriteLine(res3);
+
+            Console.WriteLine("Press any ket to continue.");
+            Console.ReadLine();
+
+        }
+    }
+}
